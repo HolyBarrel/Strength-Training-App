6de41462042282ae31ec5809eb7f2d55141a5aea
Magnus Allison <magnus.allison@tietoevry.com>
2023-07-15 23:24:22 +0200

    Created .net maui blazor app project

---
+++ StrengthTrainingApp/StrengthTrainingApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StrengthTrainingApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ StrengthTrainingApp/StrengthTrainingApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StrengthTrainingApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
