23e8dda57718724f3affb67bb592d46aedb34da8
HolyBarrel <magnus.allison@getmail.no>
2023-07-22 23:49:16 +0200

    Implemented new service methods for Set

---
+++ StrengthTrainingApp/StrengthTrainingApp/DataTransferObjects/SetEssentials.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrengthTrainingApp.DataTransferObjects
+{
+    //Credz:https://app.quicktype.io/
+    public class SetEssentials
+    {
+        [JsonProperty("exercise_id")]
+        public long ExerciseId { get; set; }
+
+        [JsonProperty("reps")]
+        public long Reps { get; set; }
+
+        [JsonProperty("weight")]
+        public long Weight { get; set; }
+    }
+}
---
+++ StrengthTrainingApp/StrengthTrainingApp/Services/Implementations/SetService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using StrengthTrainingApp.DataTransferObjects;
+using StrengthTrainingApp.Services.Interfaces;
+using StrengthTrainingApp.Utility;
+
+namespace StrengthTrainingApp.Services.Implementations
+{
+    public class SetService : ServiceBase, ISetService
+    {
+        public SetService(HttpClient client) : base(client) { }
+
+        /**
+         * curl -X POST 'https://xpyatpstjihpnjgujeae.supabase.co/rest/v1/sets' \
+-H "apikey: SUPABASE_KEY" \
+-H "Authorization: Bearer SUPABASE_KEY" \
+-H "Content-Type: application/json" \
+-d '[{ "some_column": "someValue" }, { "other_column": "otherValue" }]'
+        */
+        public async Task CreateSets(List<SetEssentials> sets)
+        {
+            EnsureSetup();
+
+            /*
+             *
+             * EXAMPLE BODY:
+            [
+                {
+                    "exercise_id": 3,
+                    "reps": 12,
+                    "weight": 60
+                },
+                {
+                    "exercise_id": 3,
+                    "reps": 8,
+                    "weight": 85
+                }
+            ]
+             */
+            var response = await Client.PostAsync("/rest/v1/sets", JsonHelper.SerializeContent(sets));
+
+            if (!response.IsSuccessStatusCode)
+            {
+
+                throw new Exception("Failed to create sets.");
+
+            }
+        }
+    }
+}
